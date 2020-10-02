a41b3517d0959d958998d80bb3173e0e5b832f6f
imzoughene <imzoughene@protonmail.com>
2020-10-02 11:02:13 +0100

    delete post

--- .history/app/controllers/posts_controller_20201002105554.rb
+++ .history/app/controllers/posts_controller_20201002105554.rb
@@ -41,7 +41,6 @@ class PostsController < ApplicationController
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
-        redirect_to posts_path
     end
 
     private def post_params
